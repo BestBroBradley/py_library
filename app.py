01ebe295ce0b0ccf5dab4a8122ffd897eb934d7c
Allison Bradley <allison.bradley@outlook.com>
2020-07-07 14:09:36 -0600

    Wired up user interface to add book

--- app.py
+++ app.py
@@ -1,3 +1,48 @@
 import utils.operations as operations
 
-operations.add_book("Holes", "Louis Sachar")
+welcome = '''
+    Welcome to your library!
+'''
+
+menu = '''
+    Would you like to:
+-View your library (view)
+-Add a book to your library (add)
+-Delete a book from your library (delete)
+-Mark a book as read (update)
+-Exit (exit)
+
+'''
+
+goodbye = '''
+    Thank you for using our service.
+'''
+
+
+def initiate():
+    print(welcome)
+    selection = input(menu)
+    switchboard[selection]()
+
+
+def add():
+    title = input("What title would you like to add? ")
+    author = input(f"Who wrote {title}? ")
+    confirm = input(f"{title} by {author} will be added to your library.  Is this ok? (y/n) ")
+    if confirm == "y":
+        operations.add_book(title, author)
+        print("Added to library!")
+    repeat()
+
+
+def repeat():
+    selection = input(menu)
+    switchboard[selection]()
+
+
+switchboard = {
+    "view": operations.open_file,
+    "add": add
+}
+
+initiate()
