f69aba5eb57a0408dc4689a6ab24bf9814b54d88
Allison Bradley <allison.bradley@outlook.com>
2020-07-07 11:42:35 -0600

    Working on building out structure

---
+++ app.py
@@ -0,0 +1,3 @@
+import utils.operations as operations
+
+operations.add_book("Holes", "Louis Sachar")
---
+++ utils/operations.py
@@ -0,0 +1,19 @@
+def open_file():
+    pass
+
+
+def write_file(library):
+    pass
+
+
+def add_book(title, author):
+    print("Add")
+
+
+def delete_book(title):
+    pass
+
+
+def update_book(title):
+    pass
+
