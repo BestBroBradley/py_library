8de378336fc4f23cbd71f3b96bcb65934712653a
Allison Bradley <allison.bradley@outlook.com>
2020-07-07 12:26:56 -0600

    Added readfile, writefile, and addbook functionality

--- utils/operations.py
+++ utils/operations.py
@@ -1,13 +1,22 @@
+import json
+
+
 def open_file():
-    pass
+    with open("utils/library.txt", "r") as file:
+        library = json.load(file)
+    return library
 
 
 def write_file(library):
-    pass
+    with open("utils/library.txt", "w") as file:
+        json.dump(library, file)
 
 
 def add_book(title, author):
-    print("Add")
+    library = open_file()
+    print(library)
+    library.append({"title": title, "author": author, "is_read": False})
+    write_file(library)
 
 
 def delete_book(title):
