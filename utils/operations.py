6e4faf422040f83c769cc392fee1fd90b19f46e9
Allison Bradley <allison.bradley@outlook.com>
2021-01-15 12:42:16 -0700

    Incorporated context manager

--- utils/operations.py
+++ utils/operations.py
@@ -1,67 +1,51 @@
-import sqlite3
+from .database_connection import DatabaseConnection
 
 
 def create_file():
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS library(title text primary key, author text, read integer)")
-
-    connection.commit()
-    connection.close()
+        cursor.execute("CREATE TABLE IF NOT EXISTS library(title text primary key, author text, read integer)")
 
 
 def view_all():
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("SELECT title, author, read FROM library")
-    library = [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
+        cursor.execute("SELECT title, author, read FROM library")
+        library = [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
 
-    connection.close()
     return library
 
 
 def add_book(title, author):
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("INSERT INTO library VALUES (?, ?, 0)", (title, author))
-
-    connection.commit()
-    connection.close()
+        cursor.execute("INSERT INTO library VALUES (?, ?, 0)", (title, author))
 
 
 def delete_book(title):
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT title, author FROM library WHERE title = ?", (title,))
-    book = cursor.fetchone()
-    if book:
-        cursor.execute("DELETE FROM library WHERE title = ?", (title,))
-
-        connection.commit()
-        connection.close()
-        return print("Successfully deleted.")
-    connection.close()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT title, author FROM library WHERE title = ?", (title,))
+        book = cursor.fetchone()
+        if book:
+            cursor.execute("DELETE FROM library WHERE title = ?", (title,))
+            return print("Successfully deleted.")
     return print("Item not in library.")
 
 
 def update_book(title):
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("SELECT title, author FROM library WHERE title = ?", (title,))
-    book = cursor.fetchone()
-    if book:
-        cursor.execute("UPDATE library SET read = 1 WHERE title = ?", (title,))
-
-        connection.commit()
-        connection.close()
-        return print("Successfully updated.")
-    connection.close()
-    return print("Item not in library.")
+        cursor.execute("SELECT title, author FROM library WHERE title = ?", (title,))
+        book = cursor.fetchone()
+        if book:
+            cursor.execute("UPDATE library SET read = 1 WHERE title = ?", (title,))
+            return print("Successfully updated.")
+        return print("Item not in library.")
 
 
 create_file()
